d9579c205b9d74f270b137c7297086c888bec73c
AustrianPenguin <charley.cummings@aol.com>
2026-01-09 16:33:47 +0000

    feat: add Homomorphism class with initialisation data

--- homomorphisms.py
+++ homomorphisms.py
@@ -7,11 +7,17 @@ import networkx as nx
 
 class Homomorphism:
     def __init__(self,
-                 domain : ModuleDiagram,
-                 image : ModuleDiagram,
-                 codomain : ModuleDiagram):
+                 domain: ModuleDiagram,
+                 codomain: ModuleDiagram,
+                 mapping: dict[Vertex, Vertex]): 
+        self.domain = domain
+        self.codomain = codomain
+        self.mapping = mapping
+        self.index_mapping = {self.domain.vertex_to_index[k] : self.codomain.vertex_to_index[v]
+                                for k, v in self.mapping.items()}
+        self._check_validity()
         
-        
+        self.dimension = len(self.mapping)
         self.domain = domain
         self.image = image
         self.codomain = codomain
