c1a81ae849b5b63621f00759f339732e053bec82
AustrianPenguin <charley.cummings@aol.com>
2026-01-09 16:33:24 +0000

    style: update and tidy up file imports

--- homomorphisms.py
+++ homomorphisms.py
@@ -1,4 +1,9 @@
-from modulediagram import ModuleDiagram
+from bitmask_subgraph import Vertex, Arrow
+from modulediagram import ModuleDiagram, ModuleSubDiagram
+import time
+import numpy as np
+from numpy.typing import NDArray
+import networkx as nx
 
 class Homomorphism:
     def __init__(self,
