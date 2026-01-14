62745cc8c1acf133bca24e1f52a30965d8effe6d
AustrianPenguin <charley.cummings@aol.com>
2026-01-14 14:23:40 +0000

    refactor: examples to use new Path initialisation data

--- B_quiver_algebra.py
+++ B_quiver_algebra.py
@@ -299,34 +299,34 @@ if __name__ == "__main__":
     examples = Examples({})
 
     examples.add(("Type A no relations",
-                  MonomialQuiverAlgebra(arrows=[Arrow(0,1,"a"),
-                                                Arrow(1,2,"b"),
-                                                Arrow(2,3,"c")],
+                  MonomialQuiverAlgebra(arrows=[Arrow(Vertex(0),Vertex(1),"a"),
+                                                Arrow(Vertex(1),Vertex(2),"b"),
+                                                Arrow(Vertex(2),Vertex(3),"c")],
                                         relations = [])))
     
     examples.add(("Type A rad2 relations",
-                  MonomialQuiverAlgebra(arrows=[Arrow(0,1,"a"),
-                                                Arrow(1,2,"b"),
-                                                Arrow(2,3,"c")],
-                                        relations = [Path((Arrow(0,1,"a"),
-                                                           Arrow(1,2,"b"))),
-                                                     Path((Arrow(1,2,"b"),
-                                                           Arrow(2,3,"c")))])))
+                  MonomialQuiverAlgebra(arrows=[Arrow(Vertex(0),Vertex(1),"a"),
+                                                Arrow(Vertex(1),Vertex(2),"b"),
+                                                Arrow(Vertex(2),Vertex(3),"c")],
+                                        relations = [Path((Arrow(Vertex(0),Vertex(1),"a"),
+                                                           Arrow(Vertex(1),Vertex(2),"b"))),
+                                                     Path((Arrow(Vertex(1),Vertex(2),"b"),
+                                                           Arrow(Vertex(2),Vertex(3),"c")))])))
 
     examples.add(("Three cyclic",
-                 MonomialQuiverAlgebra(arrows=[Arrow(0,1,"a"),
-                                               Arrow(1,2,"b"),
-                                               Arrow(2,0,"c")],
-                                       relations=[Path((Arrow(0,1,"a"),
-                                                        Arrow(1,2,"b")))])))
+                 MonomialQuiverAlgebra(arrows=[Arrow(Vertex(0),Vertex(1),"a"),
+                                               Arrow(Vertex(1),Vertex(2),"b"),
+                                               Arrow(Vertex(2),Vertex(0),"c")],
+                                       relations=[Path((Arrow(Vertex(0),Vertex(1),"a"),
+                                                        Arrow(Vertex(1),Vertex(2),"b")))])))
 
     examples.run()
 
-    qa = MonomialQuiverAlgebra(arrows=[Arrow(0,1,"a"),
-                                       Arrow(1,2,"b"),
-                                       Arrow(2,3,"c")],
-                               relations = [Path((Arrow(0,1,"a"),
-                                                  Arrow(1,2,"b"))),
-                                            Path((Arrow(1,2,"b"),
-                                                  Arrow(2,3,"c")))])
+    qa = MonomialQuiverAlgebra(arrows=[Arrow(Vertex(0),Vertex(1),"a"),
+                                       Arrow(Vertex(1),Vertex(2),"b"),
+                                       Arrow(Vertex(2),Vertex(3),"c")],
+                               relations = [Path((Arrow(Vertex(0),Vertex(1),"a"),
+                                                  Arrow(Vertex(1),Vertex(2),"b"))),
+                                            Path((Arrow(Vertex(1),Vertex(2),"b"),
+                                                  Arrow(Vertex(2),Vertex(3),"c")))])
     
